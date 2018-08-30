30d1f56ca12a2cdbddea8d86bd96759d741a3c2b
Luca Freschi <luca_freschi@hms.harvard.edu>
2018-08-30 16:08:15 -0400

    * Using os.scandir instead of glob to get the list of the directories faster. Then I sort them by name. (megapipe-generate-reports.py)

--- bin/megapipe-generate-reports.py
+++ bin/megapipe-generate-reports.py
@@ -1,36 +1,37 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-import glob
 import sys
 import os.path
+from os import scandir
+import argparse
 
-if(len(sys.argv) != 2):
-    print(":: usage: {} <results_dir> ".format(sys.argv[0]))
-    sys.exit()
+parser = argparse.ArgumentParser()
+parser.add_argument("res_dir", type=str,
+					help="directory with the megapipe results")
+parser.add_argument("tsv_out", type=str,
+					help="output file (TSV)")
+args = parser.parse_args()
 
-res_dir=sys.argv[1]
-
-l_dirs=glob.glob(res_dir+"/*")
-
-print("xref\tlineage\tvcf\tassembly")
-
-for d in sorted(l_dirs):
-    vcf="no"
-    assembly="no"
-    lineage="no"
-    d2=d.split("/")[-1]
-    #I get the lineage
-    if os.path.isfile(res_dir+"/"+d2+"/fast-lineage-caller/"+d2+".lineage"):
-        with open(res_dir+"/"+d2+"/fast-lineage-caller/"+d2+".lineage","r") as inp:
-            line=inp.readline()
-            entry=line.rstrip("\n").split(" ")
-            lineage=entry[1]
-    #I check if the vcf is present
-    if os.path.isfile(res_dir+"/"+d2+"/pilon/"+d2+".vcf"):
-        vcf="YES"
-    #I check if the assembly is present
-    if os.path.isfile(res_dir+"/"+d2+"/spades/"+d2+"_scaffolds.fasta"):
-        assembly="YES"
-    print("\t".join([d2,lineage,vcf,assembly]))
-
-
+d_isolates={}
+l_dirs = os.scandir(args.res_dir)
+with open(args.tsv_out,"w") as outf:
+    outf.write("xref\tlineage\tvcf\tassembly\n")
+    for d in l_dirs:
+        vcf="no"
+        assembly="no"
+        lineage="no"
+        #I get the lineage
+        if os.path.isfile(args.res_dir+"/"+d.name+"/fast-lineage-caller/"+d.name+".lineage"):
+            with open(args.res_dir+"/"+d.name+"/fast-lineage-caller/"+d.name+".lineage","r") as inp:
+                line=inp.readline()
+                entry=line.rstrip("\n").split(" ")
+                lineage=entry[1]
+        #I check if the vcf is present
+        if os.path.isfile(args.res_dir+"/"+d.name+"/pilon/"+d.name+".vcf"):
+            vcf="YES"
+        #I check if the assembly is present
+        if os.path.isfile(args.res_dir+"/"+d.name+"/spades/"+d.name+"_scaffolds.fasta"):
+            assembly="YES"
+        d_isolates[d.name]="\t".join([d.name,lineage,vcf,assembly])+"\n"
+    for isolate in sorted(d_isolates):
+        outf.write(d_isolates[isolate])
